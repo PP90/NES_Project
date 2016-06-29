52b2d3c1831b964c2f05c368d1c4a97c2cf673e3
pp90 <pietropiscione@gmail.com>
2016-06-29 23:15:12 +0200

    Added two file in order to compute the total energy consumed by the device. It works not so good because in the Cooja simulator some values are negative. To fix them

---
+++ energy-data-structure.c
@@ -0,0 +1,21 @@
+
+#ifndef ENERGY_DATA_STRUCTURE_C 
+#define ENERGY_DATA_STRUCTURE_C 4332
+
+#include "energy-data-structure.h"
+
+static uint32_t 
+total_energy_consumed(void)
+{	
+	struct energy_data_structure *my_struct;
+	uint32_t total_energy;
+	my_struct->energy_lpm = energest_type_time(ENERGEST_TYPE_LPM);
+	my_struct->energy_cpu = energest_type_time(ENERGEST_TYPE_CPU);
+	my_struct->energy_rx = energest_type_time(ENERGEST_TYPE_LISTEN);
+	my_struct->energy_tx = energest_type_time(ENERGEST_TYPE_TRANSMIT);
+	my_struct->energy_rled = energest_type_time(ENERGEST_TYPE_LED_RED);
+	total_energy=my_struct->energy_lpm+my_struct->energy_cpu+my_struct->energy_rx+my_struct->energy_tx+my_struct->energy_rled;
+	return total_energy;
+}
+
+#endif
